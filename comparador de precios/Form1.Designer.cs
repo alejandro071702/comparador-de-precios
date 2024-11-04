e5005011ab039cf4a480fa522a5c75a2bc1b4e0a
Francisco Jesus Hernandez Hernandez <a353691@alumnos.uaslp.mx>
2024-11-03 22:32:41 -0600

    LGTM!

--- comparador de precios/Form1.Designer.cs
+++ comparador de precios/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.PriceList = new System.Windows.Forms.ListBox();
+            this.NameList = new System.Windows.Forms.ListBox();
+            this.SellerList = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(214, 42);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Buscar";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // PriceList
+            // 
+            this.PriceList.FormattingEnabled = true;
+            this.PriceList.Location = new System.Drawing.Point(12, 120);
+            this.PriceList.Name = "PriceList";
+            this.PriceList.Size = new System.Drawing.Size(121, 225);
+            this.PriceList.TabIndex = 2;
+            // 
+            // NameList
+            // 
+            this.NameList.FormattingEnabled = true;
+            this.NameList.Location = new System.Drawing.Point(169, 120);
+            this.NameList.Name = "NameList";
+            this.NameList.Size = new System.Drawing.Size(360, 225);
+            this.NameList.TabIndex = 3;
+            // 
+            // SellerList
+            // 
+            this.SellerList.FormattingEnabled = true;
+            this.SellerList.Location = new System.Drawing.Point(569, 120);
+            this.SellerList.Name = "SellerList";
+            this.SellerList.Size = new System.Drawing.Size(126, 225);
+            this.SellerList.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(569, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 56);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(376, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mejor Oferta:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.SellerList);
+            this.Controls.Add(this.NameList);
+            this.Controls.Add(this.PriceList);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ListBox PriceList;
+        private System.Windows.Forms.ListBox NameList;
+        private System.Windows.Forms.ListBox SellerList;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- comparador de precios/Form1.cs
+++ comparador de precios/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,232 @@ namespace comparador_de_precios
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool doingQuery = false;
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (doingQuery) return;
+            doingQuery = true;
+            query(textBox1.Text);
+            NameList.Items.Clear();
+            PriceList.Items.Clear();
+            SellerList.Items.Clear();
+        }
+
+        string bodySEO = "";
+        string pageBody = "";
+        async void query(string thing)
+        {
+            bodySEO = "";
+            pageBody = "";
+            //await WebRequestSEO(@"https://google.com/search?q=", thing, "%20", "&sclient=products-cc&udm=28");
+            listBox1.Items.Clear();
+            await WebRequestSEO(@"https://google.com/search?q=", thing + "Mercado Libre", "%20", "&sclient=products-cc&udm=28");
+            showProducts(bodySEO, "Mercado Libre");
+            await WebRequestSEO(@"https://google.com/search?q=", thing + "Cyberpuerta", "%20", "&sclient=products-cc&udm=28");
+            showProducts(bodySEO, "Cyberpuerta");
+            await WebRequestSEO(@"https://google.com/search?q=", thing + "Walmart", "%20", "&sclient=products-cc&udm=28");
+            showProducts(bodySEO, "Walmart");
+            //showMercadoLibre(bodySEO);
+
+            string price = PriceList.Items[0].ToString().Replace(",", "");
+            price = price.Substring(4);
+            double bestPrice = double.Parse(price);
+            int bestIndex = 0;
+            for (int i = 1; i < PriceList.Items.Count; i++)
+            {
+                price = PriceList.Items[i].ToString().Replace(",", "");
+                price = price.Substring(4);
+                double dPrice = double.Parse(price);
+                if (bestPrice > dPrice)
+                {
+                    bestIndex = i;
+                    bestPrice = dPrice;
+                }
+            }
+
+            listBox1.Items.Add(NameList.Items[bestIndex]);
+            listBox1.Items.Add(PriceList.Items[bestIndex]);
+            listBox1.Items.Add(SellerList.Items[bestIndex]);
+            doingQuery = false;
+        }
+
+        static readonly HttpClient client = new HttpClient();
+
+        async Task WebRequestSEO(string seo, string query, string space = "%20", string flags = "")
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            string url = seo + query.Replace(" ", space) + flags;
+            Console.WriteLine(url);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                using (StreamWriter outputFile = new StreamWriter("Google.html"))
+                {
+                    outputFile.WriteLine(responseBody);
+                }
+                bodySEO = responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                //Console.WriteLine("\nException Caught!");
+                //Console.WriteLine("Message :{0} ", e.Message);
+                //MessageBox.Show("Page not found", "Alert");
+                return;
+            }
+            Console.WriteLine("Finish" + url);
+        }
+
+        async Task WebRequest(string url)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            Console.WriteLine(url);
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                pageBody = responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                //Console.WriteLine("\nException Caught!");
+                //Console.WriteLine("Message :{0} ", e.Message);
+                //MessageBox.Show("Page not found", "Alert");
+                return;
+            }
+            Console.WriteLine("Finish" + url);
+        }
+
+        void showProducts(string responseBody)
+        {
+            NameList.Items.Clear();
+            PriceList.Items.Clear();
+            SellerList.Items.Clear();
+
+            for (int startIndex = responseBody.IndexOf(@"xcR77""><d");
+                startIndex != -1;
+                startIndex = responseBody.IndexOf("xcR77", startIndex + 1))
+            {
+                //Product Name
+                int startName = responseBody.IndexOf("rgHvZc", startIndex) + "rgHvZc".Length + 3;
+                int endName = responseBody.IndexOf("</", startName);
+                startName = responseBody.LastIndexOf(">", endName) + 1;
+
+                string name = responseBody.Substring(startName, endName - startName);
+
+                if (name.IndexOf("Google") != -1) continue;
+
+                //Cost
+                int startPrice = responseBody.IndexOf("HRLxBb", startIndex);
+                int endPrice = responseBody.IndexOf("</", startPrice);
+                startPrice = responseBody.LastIndexOf(">", endPrice) + 1;
+
+                string price = responseBody.Substring(startPrice, endPrice - startPrice);
+
+                if (price.IndexOf("mensuales") != price.IndexOf("impuestos")) continue;
+
+                //Seller
+                int startSeller = responseBody.IndexOf("> ", endPrice) + 2;
+                int endSeller = responseBody.IndexOf("<", startSeller);
+
+                string seller = responseBody.Substring(startSeller, endSeller - startSeller);
+
+                Console.WriteLine("{0}\r\n{1}\r\n{2}", seller, name, price);
+
+                NameList.Items.Add(name);
+                PriceList.Items.Add(price);
+                SellerList.Items.Add(seller);
+            }
+        }
+
+        void showProducts(string responseBody, string sellerValidator)
+        {
+            //NameList.Items.Clear();
+            //PriceList.Items.Clear();
+            //SellerList.Items.Clear();
+
+            for (int startIndex = responseBody.IndexOf(@"xcR77""><d");
+                startIndex != -1;
+                startIndex = responseBody.IndexOf("xcR77", startIndex + 1))
+            {
+                //Product Name
+                int startName = responseBody.IndexOf("rgHvZc", startIndex) + "rgHvZc".Length + 3;
+                int endName = responseBody.IndexOf("</", startName);
+                startName = responseBody.LastIndexOf(">", endName) + 1;
+
+                string name = responseBody.Substring(startName, endName - startName);
+
+                if (name.IndexOf("Google") != -1) continue;
+
+                //Cost
+                int startPrice = responseBody.IndexOf("HRLxBb", startIndex);
+                int endPrice = responseBody.IndexOf("</", startPrice);
+                startPrice = responseBody.LastIndexOf(">", endPrice) + 1;
+
+                string price = responseBody.Substring(startPrice, endPrice - startPrice);
+
+                if (price.IndexOf("mensuales") != price.IndexOf("impuestos")) continue;
+
+                //Seller
+                int startSeller = responseBody.IndexOf("> ", endPrice) + 2;
+                int endSeller = responseBody.IndexOf("<", startSeller);
+
+                string seller = responseBody.Substring(startSeller, endSeller - startSeller);
+
+                if (seller.IndexOf(sellerValidator) == -1) continue;
+
+                Console.WriteLine("{0}\r\n{1}\r\n{2}", seller, name, price);
+
+                NameList.Items.Add(name);
+                PriceList.Items.Add(price);
+                SellerList.Items.Add(seller);
+            }
+        }
+        async void showMercadoLibre(string responseBody)
+        {            
+            for (int start = bodySEO.IndexOf(@"https://articulo.mercadolibre.com.mx/"); start != -1; start = bodySEO.IndexOf(@"https://articulo.mercadolibre.com.mx/", start + 1))
+            {
+                int startURL = start;
+                int endURL = bodySEO.IndexOf(@"%3", start + 1);
+
+                string url = bodySEO.Substring(startURL, endURL - startURL);
+
+                await WebRequest(url);
+
+                //Name
+                int nameStart = pageBody.IndexOf("ui-pdp-title");
+                int nameEnd = pageBody.IndexOf("</", nameStart);
+                nameStart = pageBody.LastIndexOf(">", nameEnd);
+
+                string name = pageBody.Substring(nameStart, nameEnd - nameStart);
+
+                //Price
+                int priceStart = pageBody.IndexOf("itemprop=price");
+                if (priceStart == -1) continue;
+                int priceEnd = pageBody.IndexOf("\">", priceStart);
+                priceStart = pageBody.LastIndexOf("\"", priceEnd);
+
+                string price = pageBody.Substring(priceStart, priceEnd - priceStart);
+
+                NameList.Items.Add(name);
+                PriceList.Items.Add(price);
+                SellerList.Items.Add(" en Mercado Libre");
+
+                
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
